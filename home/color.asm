bbfd2c46f135188cf1688acdae2ee55030dcc039
AtmaBuster <ultiomos@gmail.com>
2024-11-30 11:40:56 -0600

    Added another color utility routine

--- home/color.asm
+++ home/color.asm
@@ -42,3 +42,28 @@ CopyPals:
 	dec b
 	jr nz, .pal_loop
 	ret
+
+; ================================================
+; ClearPalettes
+; ================================================
+; Sets all BG and OBJ palettes to white (31, 31, 31)
+; ================================================
+ClearPalettes::
+	call CheckColorHardware
+	ret z
+	ld c, LOW(rBGPI)
+	call .ClearPals
+	ld c, LOW(rOBPI)
+.ClearPals:
+	ld a, (1 << rBGPI_AUTO_INCREMENT)
+	ldh [c], a
+	inc c
+	ld b, 32 ; colors
+.clear_loop
+	ld a, $FF ; LOW(RGB 31, 31, 31)
+	ldh [c], a
+	ld a, $7F ; HIGH(RGB 31, 31, 31)
+	ldh [c], a
+	dec b
+	jr nz, .clear_loop
+	ret
